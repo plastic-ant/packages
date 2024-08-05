229ee7a67d9cd4c41645cf40775b816bf6306c8f
pas-mike <michael.devereux@plasticant.co.uk>
2024-08-05 13:52:32 +0000

    chore(nx-cdktf): added output test

--- packages/nx-cdktf/src/index.spec.ts
+++ packages/nx-cdktf/src/index.spec.ts
@@ -86,79 +86,67 @@ describe("nx-cdktf", () => {
     ]);
   });
 
-  // it("should create nodes based on cdktf.json with output cache", async () => {
-  //   vol.fromJSON({
-  //     "proj/cdktf.json": `{ "output": "cdktf.test.out" }`,
-  //     "proj/project.json": "{}",
-  //   });
+  it("should create nodes based on cdktf.json with output cache", async () => {
+    vol.fromJSON({
+      "proj/cdktf.json": `{ "output": "cdktf.test.out" }`,
+      "proj/project.json": "{}",
+    });
 
-  //   const results = await createNodesFunction(
-  //     ["proj/cdktf.json"],
-  //     {
-  //       synthTargetName: "synth-test",
-  //       deployTargetName: "deploy-test",
-  //       getTargetName: "get-test",
-  //     },
-  //     context
-  //   );
+    const results = await createNodesFunction(
+      ["proj/cdktf.json"],
+      {
+        synthTargetName: "synth-test",
+        deployTargetName: "deploy-test",
+        getTargetName: "get-test",
+      },
+      context
+    );
 
-  //   expect(results).toMatchObject([
-  //     [
-  //       "proj/cdktf.json",
-  //       {
-  //         projects: {
-  //           proj: {
-  //             metadata: undefined,
-  //             root: "proj",
-  //             targets: {
-  //               "get-test": {
-  //                 cache: true,
-  //                 command: "cdktf get",
-  //                 inputs: ["production", "^production", { externalDependencies: ["cdktf-cli"] }],
-  //                 options: { cwd: "proj" },
-  //                 outputs: ["{projectRoot}/cdktf.out"],
-  //               },
-  //               "deploy-test": {
-  //                 cache: true,
-  //                 command: "cdktf deploy",
-  //                 inputs: ["production", "^production", { externalDependencies: ["cdktf-cli"] }],
-  //                 metadata: {
-  //                   help: {
-  //                     command: "yarn cdktf deploy --help",
-  //                     example: {
-  //                       options: {
-  //                         "require-approval": "never",
-  //                       },
-  //                     },
-  //                   },
-  //                   technologies: ["cdktf"],
-  //                 },
-  //                 options: { cwd: "proj" },
-  //                 outputs: ["{projectRoot}/cdktf.test.out"],
-  //               },
-  //               "synth-test": {
-  //                 cache: true,
-  //                 command: "cdktf synth",
-  //                 inputs: ["production", "^production", { externalDependencies: ["cdktf-cli"] }],
-  //                 metadata: {
-  //                   help: {
-  //                     command: "yarn cdktf synth --help",
-  //                     example: {
-  //                       options: {
-  //                         strict: true,
-  //                       },
-  //                     },
-  //                   },
-  //                   technologies: ["cdktf"],
-  //                 },
-  //                 options: { cwd: "proj" },
-  //                 outputs: ["{projectRoot}/cdktf.test.out"],
-  //               },
-  //             },
-  //           },
-  //         },
-  //       },
-  //     ],
-  //   ]);
-  //});
+    expect(results).toMatchObject([
+      [
+        "proj/cdktf.json",
+        {
+          projects: {
+            proj: {
+              metadata: undefined,
+              root: "proj",
+              targets: {
+                "get-test": {
+                  cache: false,
+                  command: "cdktf get",
+                  inputs: ["production", "^production", { externalDependencies: ["cdktf-cli"] }],
+                  options: { cwd: "proj" },
+                },
+                "deploy-test": {
+                  cache: true,
+                  command: "cdktf deploy",
+                  inputs: ["production", "^production", { externalDependencies: ["cdktf-cli"] }],
+                  options: { cwd: "proj" },
+                  outputs: ["{projectRoot}/cdktf.test.out"],
+                },
+                "synth-test": {
+                  cache: true,
+                  command: "cdktf synth",
+                  inputs: ["production", "^production", { externalDependencies: ["cdktf-cli"] }],
+                  metadata: {
+                    help: {
+                      command: "yarn cdktf synth --help",
+                      example: {
+                        options: {
+                          output: "cdktf.custom.out",
+                        },
+                      },
+                    },
+                    technologies: ["cdktf"],
+                  },
+                  options: { cwd: "proj" },
+                  outputs: ["{projectRoot}/cdktf.test.out"],
+                },
+              },
+            },
+          },
+        },
+      ],
+    ]);
+  });
 });
