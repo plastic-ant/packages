183c86348982955fdade9041de5bed29ad7acafa
pas-mike <michael.devereux@plasticant.co.uk>
2025-01-06 10:40:50 +0000

    chore(repo): package updates

--- .devcontainer/post-start-command.sh
+++ .devcontainer/post-start-command.sh
@@ -3,8 +3,11 @@ sudo apt-get update && sudo apt-get -y upgrade
 
 yarn
 
+ESLINT_VERSION=$(node -p "require('./node_modules/eslint/package.json').version")
 NX_VERSION=$(node -p "require('./node_modules/nx/package.json').version")
+HUSKY_VERSION=$(node -p "require('./node_modules/husky/package.json').version")
+JS2TS_VERSION=$(node -p "require('./node_modules/json-schema-to-typescript/package.json').version")
 
-npm i -g npm nx@$NX_VERSION
+npm i -g npm nx@$NX_VERSION eslint@$ESLINT_VERSION husky@$HUSKY_VERSION json-schema-to-typescript@$JS2TS_VERSION
 
 npx husky
