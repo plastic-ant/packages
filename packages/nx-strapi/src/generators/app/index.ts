82afae1dd0e74c9fac16eaeaa3fb9e03946554e2
pas-mike <michael.devereux@plasticant.co.uk>
2025-01-14 16:07:12 +0000

    refactor(repo): pnpm cache for ghas

--- packages/nx-strapi/src/generators/app/index.ts
+++
@@ -1,8 +0,0 @@
-import { Tree } from "@nx/devkit";
-import { AppGeneratorOptions } from "./schema";
-import { spawnSync } from "child_process";
-
-export default async function (tree: Tree, options: AppGeneratorOptions) {
-  spawnSync(`npx create-strapi`, { cwd: options.directory });
-  return { success: true };
-}
