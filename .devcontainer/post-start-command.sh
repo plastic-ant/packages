7ffc8ea840c3231502f645595ff450473d29e700
pas-mike <michael.devereux@plasticant.co.uk>
2025-01-14 11:41:36 +0000

    refactor(repo): wip

--- .devcontainer/post-start-command.sh
+++ .devcontainer/post-start-command.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-pnpm install --frozen-lockfile
+sudo apt-get update && sudo apt-get -y upgrade
 
-echo "💾 Installing Global dependencies (e.g eslint, nx, husky)..."
+npm install -g npm@latest
+npm uninstall -g eslint typescript yarn
+
+pnpm install
+
+echo "### Installing correct global dependencies (eslint, nx, husky, js2ts)..."
 ESLINT_VERSION=$(node -p "require('./node_modules/eslint/package.json').version")
 NX_VERSION=$(node -p "require('./node_modules/nx/package.json').version")
 HUSKY_VERSION=$(node -p "require('./node_modules/husky/package.json').version")
 JS2TS_VERSION=$(node -p "require('./node_modules/json-schema-to-typescript/package.json').version")
 
-pnpm add --global nx@$NX_VERSION eslint@$ESLINT_VERSION husky@$HUSKY_VERSION json-schema-to-typescript@$JS2TS_VERSION
+npm i -g nx@$NX_VERSION eslint@$ESLINT_VERSION husky@$HUSKY_VERSION json-schema-to-typescript@$JS2TS_VERSION
+
+npx husky
